c3e6c0e72812616f24f35f5c04cfccabf95345b2
prashanthashok <prashanth.ashokram@gmail.com>
2019-07-01 20:56:36 -0400

    Testing cards-flex layout

---
+++ parknews-ux/src/app/news/news-cards/news-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'park-news-cards',
+  templateUrl: './news-cards.component.html',
+  styleUrls: ['./news-cards.component.css']
+})
+export class NewsCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
