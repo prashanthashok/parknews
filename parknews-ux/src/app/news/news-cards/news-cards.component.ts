c3855cf1eecb88a08eed0dd7db7550c084a28d2a
prashanthashok <prashanth.ashokram@gmail.com>
2019-07-01 22:38:17 -0400

    Added cards with headlines

--- parknews-ux/src/app/news/news-cards/news-cards.component.ts
+++ parknews-ux/src/app/news/news-cards/news-cards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { News } from 'src/app/shared/models/news';
+import { NewsApiService } from 'src/app/shared/apis/news-api.service';
 
 @Component({
   selector: 'park-news-cards',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsCardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private newsApi: NewsApiService) { }
+
+  newsList: News[];
+  news$;
+
+  
 
   ngOnInit() {
+
+    this.newsApi.getNews().subscribe(
+      news => {
+        this.newsList = news['articles'];
+        this.newsList.forEach((item, i) => {
+          item.id = i + 1;
+        })
+        console.log(news);
+        console.log(this.newsList);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.error);
+        console.log(err.name);
+        console.log(err.message);
+        console.log(err.status);
+      }
+    );
   }
 
 }
